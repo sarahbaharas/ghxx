bbd5f37b93d1f8a49a544fe64144a53b3325d5ac
Sarah Magdalena Birkeland <samap@equinor.com>
2019-03-08 15:32:04 +0100

    add issues from a list in batch

---
+++ batch_create.py
@@ -0,0 +1,20 @@
+import os
+import yaml
+import githubapimock as api
+
+config_path = os.path.expanduser("~/repo.yml")
+settings = yaml.load(open(config_path))
+
+
+org = settings["org"]
+repo = settings["repo"]
+user = settings["user"]
+token = settings["token"]
+
+new_issues = yaml.load(open("issues.yml"))
+for issue in new_issues:
+	title = issue["title"]
+	body = issue["body"]
+	api.create_issue(org, repo, user, token, title, body)	
+	print("Create issue: " + title)
+
